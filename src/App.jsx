da5b805cf998c2293370e21402a9e76b054fe34b
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2023-09-02 14:11:12 +0200

    feat: add alldata and meme states to App components and related modifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import Header from './components/Header'
 import Main from './components/Main'
 import data from './assets/data/memesdata'
 import { MemeContext } from './Context'
+import { useState } from 'react'
 
 export default function App() {
+  const [allData, setAllData] = useState(data)
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    image: getRamdomImage(),
+  })
+
   function getRamdomImage() {
-    const memes = data.data.memes
+    const memes = allData.data.memes
     const length = memes.length
     const index = Math.floor(Math.random() * length)
     return memes[index]
   }
   // console.log(data)
   return (
-    <MemeContext.Provider value={getRamdomImage}>
+    <MemeContext.Provider value={[getRamdomImage, meme, setMeme]}>
       <div className='container'>
         <Header />
         <Main />
